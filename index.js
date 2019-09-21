41ae385b08f63a90095dd459209dc0f11c385c6c
nick <njd5475@gmail.com>
2019-09-20 22:54:37 -0400

    Refactor and add loop.

--- index.js
+++ index.js
@@ -13,43 +13,26 @@ console.log(
 	)
 );
 
-const { PackageQuestioner } = require('./lib/package_questioner');
-const questioner = new PackageQuestioner();
+const { PackagePrompter } = require('./lib/package_prompter');
+const questioner = new PackagePrompter();
+const { OptionHandler } = require('./lib/option_handler');
+const handler = new OptionHandler(questioner);
 
 (async() => {
   try {
     await questioner.whichPackage();
     await questioner.readPackageFile();
-    option = await questioner.chooseOptions();
+    
+    do {
+      option = await questioner.chooseOptions();
 
-    if(option == 'Add a script tag') {
-      const scriptName = await questioner.scriptTag();
-      const command = await questioner.command(scriptName);
+      const handleFunc = handler.handlerByOption[option];
 
-      console.log(`Scriptname: ${scriptName} $ ${command}`);
-      await questioner.addScriptTag(scriptName, command)
-      await questioner.save();
-    }else if(option == 'Add keywords list') {
-      const keywords = questioner.getKeywordsList();
-      await questioner.setKeywordsList(keywords);
-      console.log(chalk.red(pkgFile.keywords));
-      await questioner.save();
-    }else if(option == 'Bump version') {
-      const { part, semver } =  questioner.getSemVerPart();
-      questioner.bumpVersionPart(part);
-      await questioner.save();
-    }else if(option == 'Remove key from package.json') {
-      const key = questioner.getKey();
-      await questioner.removeKey(key);
-      await questioner.save();
-    }else if(option == 'quit') {
-      process.exit(0);
-    }else if(option == 'View Key') {
-      const key = await questioner.getKey();
-      await questioner.viewKey(key);
-    }else{
-      console.log(`Unknown option ${option}`);
-    }
+      if(handleFunc) {
+        await handleFunc();
+      }
+
+    }while(option !== 'Quit');
   }catch(err) {
     console.log(err);
   }
